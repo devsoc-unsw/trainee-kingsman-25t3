c40846efa1a97d82a180b965c3b766b247beb37a
Nathaniel Tan <nathaniel.tan@student.unsw.edu.au>
2025-12-12 16:33:19 +1100

    updated session backend entries

--- backend/src/sessions/dto/update-session.dto.ts
+++ backend/src/sessions/dto/update-session.dto.ts
@@ -1,10 +1,19 @@
 import { PartialType } from "@nestjs/mapped-types";
 import { CreateSessionDto } from "./create-session.dto";
-import { IsBoolean, IsOptional } from "class-validator";
+import { IsBoolean, IsNumber, IsOptional } from "class-validator";
 
 export class UpdateSessionDto extends PartialType(CreateSessionDto) {
   // to be completed
   @IsOptional()
   @IsBoolean()
   done?: boolean;
+
+  @IsOptional()
+  @IsNumber()
+  duration?: number;
+
+  type?: string;
+
+  @IsOptional()
+  completedAt?: Date;
 }
--- backend/src/sessions/sessions.service.ts
+++ backend/src/sessions/sessions.service.ts
@@ -2,6 +2,7 @@ import { Injectable, NotFoundException } from "@nestjs/common";
 import { CreateSessionDto } from "./dto/create-session.dto";
 import { UpdateSessionDto } from "./dto/update-session.dto";
 import { DatabaseService } from "src/database/database.service";
+import { Prisma } from "@prisma/client";
 
 @Injectable()
 export class SessionsService {
@@ -38,20 +39,35 @@ export class SessionsService {
   }
 
   async updateSession(id: number, updateSessionDto: UpdateSessionDto) {
-    const session = await this.databaseService.session.create({
-      where: {
-        id: id,
-      },
-      data: updateSessionDto,
-    });
+    try {
+      const data: Prisma.SessionUpdateInput = {};
+      if (updateSessionDto.duration !== undefined) {
+        data.duration = updateSessionDto.duration;
+      }
 
-    if (!session) {
-      throw new NotFoundException(
-        `Failed to update session. Session ${id} not found`,
-      );
+      if (updateSessionDto.type !== undefined) {
+        data.type = updateSessionDto.type;
+      }
+
+      if (updateSessionDto.done !== undefined) {
+        data.done = updateSessionDto.done;
+      }
+
+      const session = await this.databaseService.session.update({
+        where: {
+          id: id,
+        },
+        data: updateSessionDto,
+      });
+
+      return session;
+    } catch (error: any) {
+      if (error.code === "P2025") {
+        throw new NotFoundException(
+          `Failed to update session. Session ${id} not found`,
+        );
+      }
     }
-
-    return session;
   }
 
   async deleteSession(id: number) {
