92b970e5b722a34e4c69fded31d62bc4531c5b34
Nathaniel Tan <nathaniel.tan@student.unsw.edu.au>
2025-12-17 21:23:21 +1100

    checked plantmodule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,7 +5,7 @@ import { UsersModule } from "./users/users.module";
 import { DatabaseModule } from "./database/database.module";
 import { TasksModule } from "./tasks/tasks.module";
 import { SessionsModule } from "./sessions/sessions.module";
-import { PlantsModule } from './plants/plants.module';
+import { PlantsModule } from "./plants/plants.module";
 
 @Module({
   imports: [
