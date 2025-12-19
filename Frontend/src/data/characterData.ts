6476ef16bacd6327f20492c9eac583e5e262d7b6
Jessica Liu <jessicaliu230@gmail.com>
2025-12-20 09:28:51 +1100

    Added avatar images for profile picture marketplace and created a characterData.ts file

---
+++ Frontend/src/data/characterData.ts
@@ -0,0 +1,45 @@
+
+export const CHARACTERS = [
+  {
+    id: 1,
+    name: "Farmer (Male)",
+    image: "/avatars/farmer-m.png",
+    price: 0,
+    isDefault: true,
+  },
+  {
+    id: 2,
+    name: "Farmer (Female)",
+    image: "/avatars/farmer-f.png",
+    price: 0,
+    isDefault: false, 
+  },
+  {
+    id: 3,
+    name: "Helpful Robot",
+    image: "/avatars/robot.png",
+    price: 150,
+    isDefault: false,
+  },
+  {
+    id: 4,
+    name: "Wise Tree",
+    image: "/avatars/tree.png",
+    price: 300,
+    isDefault: false,
+  },
+  {
+    id: 5,
+    name: "Magical Gnome",
+    image: "/avatars/gnome.png",
+    price: 500,
+    isDefault: false,
+  },
+   {
+    id: 6,
+    name: "Cute Chicken",
+    image: "/avatars/chicken.png",
+    price: 100,
+    isDefault: false,
+  },
+];
